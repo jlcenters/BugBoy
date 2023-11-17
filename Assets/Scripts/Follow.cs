7925e4f8e4391bca89ac75ac6686912f4e8cfb04
Jill Centers <58017334+jlcenters@users.noreply.github.com>
2023-11-17 16:52:33 -0500

    finished camera mechanic

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -13,7 +13,24 @@ public class Follow : MonoBehaviour
     [SerializeField] private FollowType followType;
 
 
-    private void Update()
+
+    [Header("New Camera Follow")]
+    [SerializeField] private GameObject targetGameObject;
+    [SerializeField] private Camera cam;
+    [Header("Offset")]
+    [SerializeField] private float yAngleMin = -120f;
+    [SerializeField] private float yAngleMax = 80f;
+    [SerializeField] private float distance = 5f;
+    [Header("Mouse Sensitivity")]
+    [SerializeField] private float sensitivityX = 4f;
+    [SerializeField] private float sensitivityY = 1f;
+    [Header("Current Values")]
+    [SerializeField] private float currentX = 0f;
+    [SerializeField] private float currentY = 0f;
+
+
+
+    /*private void Update()
     {
         switch (followType)
         {
@@ -28,6 +45,32 @@ public class Follow : MonoBehaviour
         }
 
 
-    }
+    }*/
 
+
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        //store mouse location
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //y values will not exceed specified y caps
+        currentY = Mathf.Clamp(currentY, yAngleMin, yAngleMax);
+    }
+    private void LateUpdate()
+    {
+        //offset object
+        Vector3 dir = new(0, 0, -distance);
+        //rotation based on current values
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        //camera position set based on current values
+        cam.transform.position = targetGameObject.transform.position + rotation * dir;
+        //will rotate towards new position
+        cam.transform.LookAt(targetGameObject.transform.position);
+    }
+    
 }
